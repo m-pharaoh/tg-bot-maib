8b96dc642a6e57bb9b3ae2f793abcd732bca911c
mphar <102926647+m-pharaoh@users.noreply.github.com>
2024-01-27 14:58:31 -0500

    heroku setup

---
+++ chat_api.py
@@ -0,0 +1,43 @@
+from contextlib import asynccontextmanager
+from http import HTTPStatus
+from telegram import Update
+from telegram.ext import Application, CommandHandler
+from telegram.ext._contexttypes import ContextTypes
+from fastapi import FastAPI, Request, Response
+import constants
+
+
+# Initialize python telegram bot
+ptb = (
+    Application.builder()
+    .updater(None)
+    .token(constants.TOKEN) 
+    .read_timeout(7)
+    .get_updates_read_timeout(42)
+    .build()
+)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await ptb.bot.setWebhook("test") # replace <your-webhook-url>
+    async with ptb:
+        await ptb.start()
+        yield
+        await ptb.stop()
+
+# Initialize FastAPI app (similar to Flask)
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/")
+async def process_update(request: Request):
+    req = await request.json()
+    update = Update.de_json(req, ptb.bot)
+    await ptb.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
+
+# Example handler
+async def start(update: Update, _: ContextTypes.DEFAULT_TYPE):
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text("starting...")
+
+ptb.add_handler(CommandHandler("start", start))
