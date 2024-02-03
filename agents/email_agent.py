8fa2c649e5fa07844cd393d2edad34eb881ed7e8
mphar <102926647+m-pharaoh@users.noreply.github.com>
2024-02-03 18:32:59 -0500

    use generate text method with hf

--- agents/email_agent.py
+++ agents/email_agent.py
@@ -20,29 +20,15 @@ async def email_action_agent(history: str):
 
                                     {history}"""
     
-    output = await client.post(json={
-        "inputs": email_prompt_template,
-        "parameters": {"temperature": 0.01, "top_p": 0.8, "max_new_tokens": 2000}
-    })
+    output = await client.text_generation(
+        prompt=email_prompt_template,
+        temperature=0.01,
+        top_p=0.8,
+        max_new_tokens=2000
+    )
 
-    # Convert bytes to string and decode
-    reply_str = output.decode('utf-8')
-
-    # Use json.loads to parse the JSON directly
-    parsed_data = json.loads(reply_str)
-
-    # Extract the generated_text from the parsed data
-    generated_text = parsed_data[0]['generated_text']
-
-    # Now, parse the inner JSON in generated_text
-    inner_data = json.loads(generated_text)
-
-    # Extract the email from the inner data
-    email = inner_data[0]['generated_text']
-
-    print(email)
-    
-    return email
+    print(output)
+    return output
     # async with ClientSession(connector=connector) as session:
         # output = await aiohttp.post(url=API_URL, headers=headers, json={
         #     "inputs": email_prompt_template,
